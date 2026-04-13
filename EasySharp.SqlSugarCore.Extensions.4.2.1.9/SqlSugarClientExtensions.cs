ba15a0dbbc671c1cdb4c06341fb79efc081a44df
CRX <260405901@qq.com>
2026-04-14 02:13:31 +0800

    feat: 增加[4.2.1.9, 4.3.2.4)版本兼容

---
+++ EasySharp.SqlSugarCore.Extensions.4.2.1.9/SqlSugarClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlSugar.Extensions
+{
+    internal static class SqlSugarClientExtensions
+    {
+        public static SqlSugarClient CopyContext(this SqlSugarClient client, ConnectionConfig config)
+        {
+            var newClient = new SqlSugarClient(config);
+            newClient.MappingColumns = client.Context.MappingColumns;
+            newClient.MappingTables = client.Context.MappingTables;
+            newClient.IgnoreColumns = client.Context.IgnoreColumns;
+            return newClient;
+        }
+    }
+}
